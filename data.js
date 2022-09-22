47c2d884f22b51eeaf0b4b178f2603e47e8be13f
ademola12345 <87110640+ademola12345@users.noreply.github.com>
2022-09-22 19:23:52 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://nft:vishu@paid1.qldpv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'5523762804:AAHhF4iqaL6oHhBQI8lJ4aSVvGKZmKRLapA',//bot token
+
+bot_name:'ttautotbot',//bot username without @
+
+bot_admin:1454608438,// admin telegram id
+
+channelsList : ['@airdrop_ultimate'],// channels list
+
+channelscheck : ['@airdrop_ultimate'],
+
+reffer_bonus:1,// refferal bonus amount
+
+min_wd:1,//min withdrawal amount
+
+daily_bonus:5000000000,//,// daily bonus amount
+
+heroku:'ttautotbot',//enter your heroku app name
+
+payment_channel:'@ttpays1'// payment channel username
+
+}
